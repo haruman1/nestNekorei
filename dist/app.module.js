1cece043909ad645a19eafcf98ce902d27c9b7ec
Haruman Wiguna <harumanwiguna1@gmail.com>
2025-09-27 16:56:36 +0700

    Switch to SQLite with dynamic file creation for TypeORM
    Replaces environment-based database configuration with SQLite for both main and backup connections. Adds logic to create SQLite files in a platform-aware location if they do not exist, improving compatibility with environments like Vercel. Removes unused module imports and static configuration for a leaner app.module.js.

--- dist/app.module.js
+++ dist/app.module.js
@@ -1,56 +1,78 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const typeorm_1 = require("@nestjs/typeorm");
-const users_module_1 = require("./users/users.module");
-const auth_module_1 = require("./auth/auth.module");
-const products_module_1 = require("./products/products.module");
 const user_entity_1 = require("./users/user.entity");
 const product_entity_1 = require("./products/entity/product.entity");
+const path = __importStar(require("path"));
+const fs = __importStar(require("fs"));
 const category_entity_1 = require("./products/entity/category.entity");
-const config_1 = require("@nestjs/config");
-const orders_module_1 = require("./orders/orders.module");
 const order_entity_1 = require("./orders/order.entity");
-const serve_static_1 = require("@nestjs/serve-static");
-const payment_service_1 = require("./payment/payment.service");
-const payment_controller_1 = require("./payment/payment.controller");
-const payment_module_1 = require("./payment/payment.module");
-const invoices_service_1 = require("./invoices/invoices.service");
-const invoices_controller_1 = require("./invoices/invoices.controller");
-const invoices_module_1 = require("./invoices/invoices.module");
-const cart_module_1 = require("./cart/cart.module");
 const cart_entity_1 = require("./cart/entity/cart.entity");
 const paymentHistory_entity_1 = require("./payment/entity/paymentHistory.entity");
-const swagger_controller_1 = require("./swagger.controller");
-const path_1 = require("path");
+function initDbFile(fileName) {
+    const isVercel = !!process.env.VERCEL;
+    let baseDir = isVercel ? '/tmp' : path.join(process.cwd(), 'data');
+    const dbPath = path.join(baseDir, fileName);
+    if (!fs.existsSync(baseDir)) {
+        fs.mkdirSync(baseDir, { recursive: true });
+    }
+    if (!fs.existsSync(dbPath)) {
+        fs.writeFileSync(dbPath, '');
+        console.log(`🗄️ SQLite file dibuat: ${dbPath}`);
+    }
+    return dbPath;
+}
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
-            config_1.ConfigModule.forRoot({
-                isGlobal: true,
-            }),
-            serve_static_1.ServeStaticModule.forRoot({
-                rootPath: (0, path_1.join)(__dirname, '..', 'swagger-static'),
-                serveRoot: process.env.CHECK_DASAR === 'development' ? '/' : '/swagger',
-            }),
             typeorm_1.TypeOrmModule.forRoot({
                 name: 'default',
-                type: process.env.DATABASE_TYPE,
-                host: process.env.DATABASE_HOST,
-                port: parseInt(process.env.DATABASE_PORT),
-                username: process.env.DATABASE_USERNAME,
-                password: process.env.DATABASE_PASSWORD,
-                database: process.env.DATABASE_NAME,
+                type: 'sqlite',
+                database: initDbFile('database.sqlite'),
                 entities: [
                     cart_entity_1.Cart,
                     cart_entity_1.CartItem,
@@ -65,25 +87,12 @@ exports.AppModule = AppModule = __decorate([
             }),
             typeorm_1.TypeOrmModule.forRoot({
                 name: 'backup',
-                type: process.env.DATABASE_TYPE_BACKUP,
-                host: process.env.DATABASE_HOST_BACKUP,
-                port: parseInt(process.env.DATABASE_PORT_BACKUP),
-                username: process.env.DATABASE_USERNAME_BACKUP,
-                password: process.env.DATABASE_PASSWORD_BACKUP,
-                database: process.env.DATABASE_NAME_BACKUP,
+                type: 'sqlite',
+                database: initDbFile('backup.sqlite'),
                 entities: [product_entity_1.ProductHistory, category_entity_1.CategoryHistory, paymentHistory_entity_1.PaymentHistory, user_entity_1.UserHistory],
                 synchronize: true,
             }),
-            users_module_1.UsersModule,
-            auth_module_1.AuthModule,
-            products_module_1.ProductsModule,
-            orders_module_1.OrdersModule,
-            payment_module_1.PaymentModule,
-            invoices_module_1.InvoicesModule,
-            cart_module_1.CartModule,
         ],
-        providers: [payment_service_1.PaymentService, invoices_service_1.InvoicesService],
-        controllers: [payment_controller_1.PaymentController, invoices_controller_1.InvoicesController, swagger_controller_1.SwaggerController],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
